f1ae3c5684cf10ee72855348304080e7af708695
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-02 17:04:58 -0400

    Created other screen

---
+++ screens/otherScreen.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Text, View } from 'react-native';
+
+const OtherScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Other!</Text>
+    </View>
+  );
+}
+export default OtherScreen
