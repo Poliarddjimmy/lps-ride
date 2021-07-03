9ae6aaaad6d1288189f0c41f538762a3a7d84691
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-07-03 01:27:02 -0400

    Added logo and icon

---
+++ components/layout/logoHeaderTitle.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import styled from "styled-components/native";
+import PropTypes from "prop-types";
+import LogoSVG from "../../assets/svg/logoSVG"; 
+let logo = require('../../assets/logo.png')
+
+
+const LogoHeaderTitle = ({ width, height, thereIsLeftHeader }) => {
+  return <LogoContainer thereIsLeftHeader={thereIsLeftHeader} >
+   
+    <LogoSVG width={width} height={height} />
+    
+  </LogoContainer>
+}
+
+export default LogoHeaderTitle;
+
+LogoHeaderTitle.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+}
+
+LogoHeaderTitle.defaultProps = {
+  width: 150,
+  height: 54
+}
+
+const LogoContainer = styled.View`
+`;
